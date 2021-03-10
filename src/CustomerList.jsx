5c5b86b63501870b3dab8ec14b192cc6d2fb8595
mika1407 <koneautomaatio@hotmail.com>
2021-03-10 07:04:52 +0200

    eka

---
+++ src/CustomerList.jsx
@@ -0,0 +1,55 @@
+import React, {useState, useEffect} from 'react'
+import './App.css';
+import CustomerService from './services/customer'
+import Customer from './Customer'
+import CustomerAdd from './CustomerAdd'
+
+const CustomerList = () => {
+
+  const [customers, setCustomers] = useState([])
+  const [näytetäänkö, setNäytetäänkö] = useState(false)
+  const [search, setSearch] = useState("")
+  const [lisäysTila, setLisäystila] = useState(false)
+
+  useEffect(() => {
+    CustomerService
+    .getAll()
+    .then(data => {
+      //console.log(data)
+      setCustomers(data)     
+    })
+  }, [])
+
+  //Hakukentän onChange tapahtumankäsittelijä
+  const handleSeachInputChange = (event) => {
+    setNäytetäänkö(true)
+    setSearch(event.target.value.toLowerCase())
+  }
+
+  return (
+    <>
+    <h1 style={{ cursor: 'pointer'}} 
+      onClick={() => setNäytetäänkö(!näytetäänkö)}>customers
+    <button onClick={() => setLisäystila(true)}>Add new</button>
+    </h1>
+
+    <input value={search} onChange={handleSeachInputChange}/>
+
+    {customers && näytetäänkö === true && lisäysTila === false && customers.map(customer =>{
+      const caseInsensName = customer.companyName.toLowerCase()
+        if (caseInsensName.indexOf(search) > -1) {
+          return (
+            <Customer key={customer.customerId} customer={customer} />
+          )
+        }
+      }   
+      )
+    }
+      {!customers && <p>Loading...</p>}
+
+      {lisäysTila === true && <CustomerAdd setLisäystila={setLisäystila} />}
+    </>
+  );
+}
+
+export default CustomerList;
